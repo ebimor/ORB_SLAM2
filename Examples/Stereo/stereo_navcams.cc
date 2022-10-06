a9e4c44a684aeb755a6b5240e5924ce87d5194d4
Ebrahim Shahrivar <eshahrivar@aeryon.com>
2022-10-06 11:08:38 -0400

    navcams

--- Examples/Stereo/stereo_navcams.cc
+++ Examples/Stereo/stereo_navcams.cc
@@ -68,34 +68,34 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
-    fsSettings["LEFT.K"] >> K_l;
-    fsSettings["RIGHT.K"] >> K_r;
+    // cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
+    // fsSettings["LEFT.K"] >> K_l;
+    // fsSettings["RIGHT.K"] >> K_r;
+    //
+    // fsSettings["LEFT.P"] >> P_l;
+    // fsSettings["RIGHT.P"] >> P_r;
+    //
+    // fsSettings["LEFT.R"] >> R_l;
+    // fsSettings["RIGHT.R"] >> R_r;
+    //
+    // fsSettings["LEFT.D"] >> D_l;
+    // fsSettings["RIGHT.D"] >> D_r;
+    //
+    // int rows_l = fsSettings["LEFT.height"];
+    // int cols_l = fsSettings["LEFT.width"];
+    // int rows_r = fsSettings["RIGHT.height"];
+    // int cols_r = fsSettings["RIGHT.width"];
+    //
+    // if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
+    //         rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
+    // {
+    //     cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
+    //     return -1;
+    // }
 
-    fsSettings["LEFT.P"] >> P_l;
-    fsSettings["RIGHT.P"] >> P_r;
-
-    fsSettings["LEFT.R"] >> R_l;
-    fsSettings["RIGHT.R"] >> R_r;
-
-    fsSettings["LEFT.D"] >> D_l;
-    fsSettings["RIGHT.D"] >> D_r;
-
-    int rows_l = fsSettings["LEFT.height"];
-    int cols_l = fsSettings["LEFT.width"];
-    int rows_r = fsSettings["RIGHT.height"];
-    int cols_r = fsSettings["RIGHT.width"];
-
-    if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
-            rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
-    {
-        cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
-        return -1;
-    }
-
-    cv::Mat M1l,M2l,M1r,M2r;
-    cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,M1l,M2l);
-    cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,M1r,M2r);
+    // cv::Mat M1l,M2l,M1r,M2r;
+    // cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,M1l,M2l);
+    // cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,M1r,M2r);
 
 
     const int nImages = vstrImageLeft.size();
@@ -115,26 +115,27 @@ int main(int argc, char **argv)
     cv::Mat imLeft, imRight, imLeftRect, imRightRect;
     for(int ni=0; ni<nImages; ni++)
     {
-        // Read left and right images from file
-        imLeft = cv::imread(vstrImageLeft[ni],cv::IMREAD_GRAYSCALE);
-        imRight = cv::imread(vstrImageRight[ni],cv::IMREAD_GRAYSCALE);
 
-        if(imLeft.empty())
+        // Read left and right images from file
+        imLeftRect = cv::imread(vstrImageLeft[ni],cv::IMREAD_GRAYSCALE);
+        imRightRect = cv::imread(vstrImageRight[ni],cv::IMREAD_GRAYSCALE);
+
+        if(imLeftRect.empty())
         {
             cerr << endl << "Failed to load image at: "
                  << string(vstrImageLeft[ni]) << endl;
             return 1;
         }
 
-        if(imRight.empty())
+        if(imRightRect.empty())
         {
             cerr << endl << "Failed to load image at: "
                  << string(vstrImageRight[ni]) << endl;
             return 1;
         }
 
-        cv::remap(imLeft,imLeftRect,M1l,M2l,cv::INTER_LINEAR);
-        cv::remap(imRight,imRightRect,M1r,M2r,cv::INTER_LINEAR);
+        // cv::remap(imLeft,imLeftRect,M1l,M2l,cv::INTER_LINEAR);
+        // cv::remap(imRight,imRightRect,M1r,M2r,cv::INTER_LINEAR);
 
         double tframe = vTimeStamp[ni];
 
@@ -205,8 +206,8 @@ void LoadImages(const string &strPathLeft, const string &strPathRight, const str
         {
             stringstream ss;
             ss << s;
-            vstrImageLeft.push_back(strPathLeft + "/" + ss.str() + ".png");
-            vstrImageRight.push_back(strPathRight + "/" + ss.str() + ".png");
+            vstrImageLeft.push_back(strPathLeft + "/" + ss.str() + ".jpg");
+            vstrImageRight.push_back(strPathRight + "/" + ss.str() + ".jpg");
             double t;
             ss >> t;
             vTimeStamps.push_back(t/1e9);
--- include/System.h
+++ include/System.h
@@ -36,6 +36,10 @@
 #include "ORBVocabulary.h"
 #include "Viewer.h"
 
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
 namespace ORB_SLAM2
 {
 
